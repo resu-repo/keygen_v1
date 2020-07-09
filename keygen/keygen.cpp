9d3fe1f92a8f622416233cff72af0dcafa84689a
Resu <Resu@DESKTOP-QGHPA7E>
2020-07-09 10:32:06 +0300

    Fix "Z" problem

--- keygen/keygen.cpp
+++ keygen/keygen.cpp
@@ -1,26 +1,27 @@
 ﻿#include <iostream>
 #include<string>
+using namespace std;
 
 int main()
 {
-    std::string str;
-    printf("login\n");
-    std::cin >> str;
-    int res = 0;
-    for (int i = 0; i < str.length(); i++)
-    {
-        if (str[i] < 'A')
-        {
-            printf("Incorrect name.Use only uppercase letters, please(A - Z)\n");
-        }
-        if (str[i] >= 'Z')
-        
-            str[i] -= 0x20;
-            res += str[i];
-        
-    }
-    res ^= 0x5678;
-    res ^= 0x1234;
-    std::cout<<"Your password"<<std::endl<<res;
+	std::string str;
+	printf("login\n");
+	std::cin >> str;
+	int res = 0, i = 0;
+	if ((str[i] < 'A') || (str[i] >= 'Z'))
+	{
+		printf("Incorrect name.Use only uppercase letters, please(A - Y)\n");
+		return true;
+	}
+	else
+	{
+		for (int i = 0; i < str.length(); i++)
+		{
+			res += str[i];
+		}
+		res ^= 0x5678;
+		res ^= 0x1234;
+		std::cout << "Your password" << std::endl << res;
+		return false;
+	}
 }
-
