dea5e97ade533adc6ea788ea9e5720aa5e32873c
Resu <Resu@DESKTOP-QGHPA7E>
2020-07-09 11:13:34 +0300

    Exception handling

--- keygen/keygen.cpp
+++ keygen/keygen.cpp
@@ -4,24 +4,28 @@ using namespace std;
 
 int main()
 {
-	std::string str;
-	printf("login\n");
-	std::cin >> str;
-	int res = 0, i = 0;
-	if ((str[i] < 'A') || (str[i] >= 'Z'))
+	bool a = true;
+	while ( a )
 	{
-		printf("Incorrect name.Use only uppercase letters, please(A - Y)\n");
-		return true;
-	}
-	else
-	{
-		for (int i = 0; i < str.length(); i++)
+		printf("login\n");
+		std::string str;
+		std::cin >> str;
+		int res = 0, i = 0;
+		if ((str[i] < 'A') || (str[i] >= 'Z'))
 		{
-			res += str[i];
+			printf("Incorrect name.Use only uppercase letters, please(A - Y)\n");
+			a = true;
 		}
-		res ^= 0x5678;
-		res ^= 0x1234;
-		std::cout << "Your password" << std::endl << res;
-		return false;
+		else
+		{
+			for (int i = 0; i < str.length(); i++)
+			{
+				res += str[i];
+			}
+			res ^= 0x5678;
+			res ^= 0x1234;
+			std::cout << "Your password" << std::endl << res;
+			a = false;
+		}
 	}
 }
